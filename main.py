0838321e0eed061809d3d8708685965c15512211
Damilola Jerugba <damiisdandy@icloud.com>
2022-03-18 09:26:09 +0100

    added get spotify and youtbe music playlist

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI, HTTPException
-from ytmusicapi import YTMusic
+from utils.index import get_playlist_duration, get_playlist_source, get_track_duration
 from models.index import GetPlaylist, Playlist
-from utils.index import get_playlist_source
+from ytmusicapi import YTMusic
+import spotipy as sp
+from spotipy.oauth2 import SpotifyClientCredentials
+from fastapi import FastAPI, HTTPException
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+# setup Spotify client
+spotify_auth_manager = SpotifyClientCredentials(
+    client_id=os.getenv("SPOTIPY_CLIENT_ID"), client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"))
+spotify = sp.Spotify(auth_manager=spotify_auth_manager)
+
+# setup Youtube client
 ytmusic = YTMusic()
+
+# FASTApi app
 app = FastAPI()
 
 
@@ -13,10 +28,10 @@ async def get_playlist(data: GetPlaylist):
     playlist_data = get_playlist_source(data.url)
     if playlist_data is None:
         raise HTTPException(status_code=400, detail="playlist url is invalid")
-    if playlist_data.get("source") == 'YOUTUBE':
+    if playlist_data.source == 'YOUTUBE':
         try:
             gotten_playlist = ytmusic.get_playlist(
-                playlist_data.get("playlist_id"))
+                playlist_data.playlist_id)
             tracks = []
             for track in gotten_playlist.get("tracks"):
                 artists = ""
@@ -48,4 +63,45 @@ async def get_playlist(data: GetPlaylist):
         except Exception as e:
             raise HTTPException(
                 status_code=400, detail=f"problem getting playlist - {e}")
+    elif playlist_data.source == 'SPOTIFY':
+        try:
+            gotten_playlist = spotify.playlist(playlist_data.playlist_id)
+            tracks = []
+            total_duration = 0
+            for tr in gotten_playlist.get("tracks").get("items"):
+                track = tr.get("track")
+                artists = ""
+                total_duration += track.get("duration_ms")
+                for artist in track.get("artists"):
+                    if track.get("artists")[-1].get("name") != artist.get("name"):
+                        artists += (artist.get("name") + ", ")
+                    else:
+                        artists += artist.get("name")
+                tracks.append({
+                    "title": track.get("name"),
+                    "artists": artists,
+                    "duration": get_track_duration(track.get("duration_ms")),
+                    "thumbnail": track.get("album").get("images")[0].get("url"),
+                    "album": track.get("album").get("name"),
+                    "isExplicit": track.get("explicit"),
+                    "searchKey": track.get("name") + " " + track.get("album").get("name")
+                })
+                pass
+
+            playlist = {
+                "id": gotten_playlist.get('id'),
+                "title":  gotten_playlist.get('name'),
+                "description":  gotten_playlist.get('description'),
+                "thumbnail": gotten_playlist.get('images')[0].get("url"),
+                "author":  gotten_playlist.get('owner').get("display_name"),
+                "year":  "-",
+                "duration":  get_playlist_duration(total_duration),
+                "trackCount":  len(gotten_playlist.get("tracks").get("items")),
+                "tracks": tracks
+            }
+        except Exception as e:
+            raise HTTPException(
+                status_code=400, detail=f"problem getting playlist - {e}")
+    elif playlist_data.source == 'APPLE':
+        pass
     return playlist
